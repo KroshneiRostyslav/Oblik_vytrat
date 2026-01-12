b1006cf14c63afdd9b2eb6eaa4525f949b28690d
KroshneiRostyslav <rostyslav.kroshnei.23@pnu.edu.ua>
2026-01-12 17:47:56 +0200

    Persist expenses using localStorage

--- Oblik_vytrat/src/app/core/services/expense.servise.ts
+++ Oblik_vytrat/src/app/core/services/expense.servise.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { Expense } from '../models/expense.model';
 
 @Injectable({
@@ -6,7 +7,27 @@ import { Expense } from '../models/expense.model';
 })
 export class ExpenseService {
 
+  private readonly STORAGE_KEY = 'expenses';
   private expenses: Expense[] = [];
+  private isBrowser: boolean;
+
+  constructor(@Inject(PLATFORM_ID) platformId: object) {
+    this.isBrowser = isPlatformBrowser(platformId);
+    if (this.isBrowser) {
+      this.load();
+    }
+  }
+
+  private load(): void {
+    const data = localStorage.getItem(this.STORAGE_KEY);
+    this.expenses = data ? JSON.parse(data) : [];
+  }
+
+  private save(): void {
+    if (this.isBrowser) {
+      localStorage.setItem(this.STORAGE_KEY, JSON.stringify(this.expenses));
+    }
+  }
 
   getExpenses(): Expense[] {
     return this.expenses;
@@ -14,6 +35,12 @@ export class ExpenseService {
 
   addExpense(expense: Expense): void {
     this.expenses.push(expense);
+    this.save();
+  }
+
+  removeExpense(id: number): void {
+    this.expenses = this.expenses.filter(e => e.id !== id);
+    this.save();
   }
 
   getTotal(): number {
